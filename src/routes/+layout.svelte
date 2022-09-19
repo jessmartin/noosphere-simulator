0fa4cc1c80dcf0ac4a6a5dac7d0670ec479859a6
Jess Martin <jessmartin@gmail.com>
2022-09-19 17:41:27 -0400

    WIP: Adding notes to a notebook

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="mx-2">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let sphere = {
+		title: "Bob's notebook",
+		links: {
+			'cat-thoughts': '1',
+			'animal-notes': '3'
+		},
+		notes: {
+			'1': {
+				parentCID: null,
+				headers: {
+					title: 'Cat Thoughts'
+				},
+				contentCID: '2'
+			},
+			'3': {
+				parentCID: null,
+				headers: {
+					title: 'Animal Notes'
+				},
+				contentCID: '4'
+			}
+		},
+		names: []
+	};
+
+	// This is essentially a fake IPFS
+	let content = {
+		'2': 'I love cats. I love every kind of cat.',
+		'4': 'I have strongly felt /cat-thoughts\n\n Dogs are just okay'
+	};
+
+	let currentNote = '';
+	let currentView = 'note';
+
+	const addNote = async () => {
+		const newNote = {
+			parentCID: null,
+			headers: {
+				title: 'New Note'
+			},
+			contentCID: '5'
+		};
+		const newCID = Math.random().toString(36).substring(7);
+		sphere.links[newCID] = newNote;
+	};
+</script>
+
+<h1 class="text-4xl mb-3">Noosphere Simulator</h1>
+
+<div class="bg-slate-200">
+	{#if currentView === 'note'}
+		{#if currentNote === null || currentNote === ''}
+			<h2 class="border-b-2 p-1 px-2 border-white font-mono font-normal text-sm italic">
+				{sphere.title} • Links
+			</h2>
+			<ul>
+				{#each Object.entries(sphere.links) as [linkCID, noteCID]}
+					<li class="p-1 px-2 font-mono">
+						<a href on:click={(noteCID) => (currentNote = noteCID)}>/{linkCID}</a>
+					</li>
+				{/each}
+				<li class="p-1 px-2 font-mono">
+					<input id="noteTitle" placeholder="New note title..." type="text" class="w-3/4" />
+					<input on:click={addNote} type="submit" class="hover:cursor-pointer" value="Add Note" />
+				</li>
+			</ul>
+		{:else if currentNote !== null}
+			<h2 class="border-b-2 p-1 px-2 border-white font-mono font-normal text-sm italic">
+				{sphere.title} • {currentNote}
+			</h2>
+			<p class="p-1 px-2 font-mono">
+				{content[sphere.notes[sphere.links[currentNote]].contentCID]}
+			</p>
+		{/if}
+	{:else if currentView == ''}
+		nothing
+	{/if}
+</div>
