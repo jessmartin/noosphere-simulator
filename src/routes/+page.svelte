87b501e1787cb0969610668800ba8ba72017aa2d
Jess Martin <jessmartin@gmail.com>
2022-09-21 14:26:44 -0400

    Change to using CIDs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,62 @@
 <script>
+	import { CID } from 'multiformats/cid';
+	import * as json from 'multiformats/codecs/json';
+	import { sha256 } from 'multiformats/hashes/sha2';
+	import { base64 } from 'multiformats/bases/base64';
+
+	import { onMount } from 'svelte';
+
 	let sphere = {
 		title: "Bob's notebook",
-		links: {
-			'cat-thoughts': '1',
-			'animal-notes': '3'
-		},
-		notes: {
-			'1': {
-				parentCID: null,
-				headers: {
-					title: 'Cat Thoughts'
-				},
-				contentCID: '2'
-			},
-			'3': {
-				parentCID: null,
-				headers: {
-					title: 'Animal Notes'
-				},
-				contentCID: '4'
-			}
-		},
+		links: {},
+		notes: {},
 		names: []
 	};
 
 	// This is essentially a fake IPFS
-	let content = {
-		'2': 'I love cats. I love every kind of cat.',
-		'4': 'I have strongly felt /cat-thoughts\n\n Dogs are just okay'
+	let content = {};
+
+	const generateCidFromObject = async (data) => {
+		const bytes = json.encode(data);
+		const hash = await sha256.digest(bytes);
+		const cid = CID.create(1, json.code, hash);
+		return cid.toString(base64.encoder);
 	};
 
+	onMount(async () => {
+		let catThoughts = 'I love cats. I love every kind of cat.';
+		let catThoughtsCID = await generateCidFromObject(catThoughts);
+		let animalNotes = 'I have strongly felt /cat-thoughts\n\n Dogs are just okay';
+		let animalNotesCID = await generateCidFromObject(animalNotes);
+		content[catThoughtsCID] = catThoughts;
+		content[animalNotesCID] = animalNotes;
+
+		// Generate Seed Data
+		let catThoughtsMemo = {
+			parentCID: null,
+			headers: {
+				title: 'Cat Thoughts'
+			},
+			contentCID: catThoughtsCID // This is text - need a new CID from Text
+		};
+		const catThoughtMemoCID = await generateCidFromObject(catThoughtsMemo);
+		sphere.links['cat-thoughts'] = catThoughtMemoCID;
+		sphere.notes[catThoughtMemoCID] = catThoughtsMemo;
+
+		let animalNotesMemo = {
+			parentCID: null,
+			headers: {
+				title: 'Animal Notes'
+			},
+			contentCID: animalNotesCID // This is text - need a new CID from Text
+		};
+		const animalNotesMemoCID = await generateCidFromObject(animalNotesMemo);
+		sphere.links['animal-notes'] = animalNotesMemoCID;
+		sphere.notes[animalNotesMemoCID] = animalNotesMemo;
+	});
+
 	// View-related stuff
-	let currentNote = 'cat-thoughts';
+	let currentNote = '';
 	let currentView = 'note';
 	let createNew = false;
 
@@ -39,9 +64,8 @@
 	let newNoteTitle = '';
 	let newNoteContent = '';
 
-	const addNote = () => {
-		console.log(newNoteTitle);
-		let contentCID = crypto.randomUUID();
+	const addNote = async () => {
+		const newNoteContentCid = await generateCidFromObject(newNoteContent);
 
 		// Create the memo
 		const memo = {
@@ -50,15 +74,24 @@
 				Title: newNoteTitle,
 				Created: new Date().getTime()
 			},
-			contentCID: contentCID
+			contentCID: newNoteContentCid
 		};
-		const newCID = crypto.randomUUID();
-		sphere.notes[newCID] = memo;
-		sphere.links[newNoteTitle] = newCID;
-		content[contentCID] = newNoteContent;
+		const memoCID = await generateCidFromObject(memo);
+
+		// Add the memo and content to the sphere
+		sphere.links[newNoteTitle] = memoCID;
+		sphere.notes[memoCID] = memo;
+
+		// Add the file content to "IPFS"
+		content[newNoteContentCid] = newNoteContent;
 
 		newNoteTitle = '';
 		newNoteContent = '';
+	};
+
+	const fetchNote = (currentNote) => {
+		const noteCID = sphere.links[currentNote];
+		return sphere.notes[noteCID];
 	};
 
 	const fetchNoteContent = (currentNote) => {
@@ -123,23 +156,24 @@
 			</ul>
 		{:else if sphere.links[currentNote]}
 			<h2 class="border-b-2 p-1 px-2 border-white font-mono font-normal text-sm italic">
-				{sphere.title} • /{currentNote}
+				{sphere.title} • /{currentNote} ({sphere.links[currentNote]})
 			</h2>
-			<p class="p-1 px-2 font-mono">
+			<p class="p-1 px-2 font-mono border-white border-b-2">
 				{fetchNoteContent(currentNote)}
 			</p>
+			<h3 class="px-2 p-1 font-mono font-semibold underline">Memo</h3>
 			<div class="p-1 px-2 font-mono flex items-center">
 				<div class="bg-purple-200 border border-purple-600 rounded-full w-16 h-32 box-content ">
 					<div class="bg-purple-400 border-2 border-purple-900 rounded-full w-6 h-6 mx-5 my-2" />
 					<div class="bg-cyan-300 border-2 border-cyan-800 rounded-full w-10 h-10 m-3" />
 					<div class="bg-purple-400 border-2 border-purple-900 rounded-full w-6 h-6 mx-5 my-2" />
 				</div>
-				<div class="w-1/2 h-32 ">
-					<div class="mx-5 my-2">Parent CID: {fetchNoteContent(currentNote).parentCID}</div>
+				<div class="h-32 ">
+					<div class="mx-5 my-2">Parent CID: {fetchNote(currentNote).parentCID}</div>
 					<div class="mx-5 h-10 m-3 flex items-center">
-						Inline Headers: {fetchNoteContent(currentNote).headers}
+						Inline Headers: &#123;{Object.entries(fetchNote(currentNote).headers)}&#125;
 					</div>
-					<div class="mx-5 my-2">Content CID: {fetchNoteContent(currentNote).contentCID}</div>
+					<div class="mx-5 my-2">Content CID: {fetchNote(currentNote).contentCID}</div>
 				</div>
 			</div>
 		{:else}
